ee764803776fc0cd9a6323b0b5d546f3679b95e2
amiv.80 <amir.r.v138@gmail.com>
2023-03-04 16:22:05 +0330

    complete user app

--- users/models.py
+++ users/models.py
@@ -7,6 +7,53 @@ from django.utils import timezone
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, send_mail
 
 
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username, phone_number, email, password, is_staff, is_active, is_superuser, **extra_fields):
+        now = timezone.now()
+
+        if not username:
+            raise ValueError('the given username must be set')
+        email = self.normalize_email(email)
+        user = self.model(phone_number = phone_number,
+                          username = username,
+                          email = email,
+                          is_staff = is_staff, is_active = True,
+                          is_superuser = is_superuser,
+                          date_joined = now,
+                          **extra_fields
+                          )
+        
+        if not extra_fields.get('no_password'):
+            user.set_password(password)
+            
+        user.save(using = self._db)
+        return user
+    
+
+    def create_user(self, username=None, phone_number=None, email=None, password=None, **extra_fields):
+        if username is None:
+            if email:
+                username = email.split('@', 1)[0]
+            if phone_number:
+                username = random.choice('abcdefghijklmnopqrstuvwxyz' )+ str(phone_number)[-7:]
+            while User.objects.filter(username = username).exists():
+                username += str(random.randint(10, 99))
+        return self._create_user(username, phone_number, email, password, False, False, **extra_fields)
+    
+
+    def create_superuser(self, username, email, phone_number, password, **extra_fields):
+        return self._create_user(username, phone_number, email, password, True, True, **extra_fields)
+    
+
+    def get_by_phone_number(self, phone_number):
+        return self.get(**{'phone_number':phone_number})
+
+
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(_('username'), max_length=25, unique=True, 
                                 help_text=_('Username must be at least 4 and at most 25 characters. It does not start with numbers or symbols'),
@@ -58,3 +105,72 @@ class User(AbstractBaseUser, PermissionsMixin):
         if self.email is not None and self.email.strip() == '':
             self.email = None
         super().save(*args, **kwargs)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nick_name = models.CharField(_('nick name'), max_length=50, blank=True)
+    avatar = models.ImageField(models.ImageField(_('avatar'), blank= True))
+    birthday = models.DateField(_('birthday'), null=True, blank=True)
+    gender = models.NullBooleanField(_('gender'))
+    province = models.ForeignKey(verbose_name=_('province'), to='Province', null=True, blank=True, on_delete=models.SET_DEFAULT)
+
+    class Meta:
+        db_table = _('user_profiles')
+        verbose_name = _('profile')
+        verbose_name_plural = _('profiles')
+
+
+    @property
+    def get_first_name(self):
+        return self.user.first_name
+    
+
+    @property
+    def get_last_name(self):
+        return self.user.last_name
+
+    
+    @property
+    def get_nick_name(self):
+        return self.user.nick_name if self.nick_name else self.user.username
+    
+
+
+class Device(models.Model):
+    WEB = 1
+    IOS = 2
+    ANDROID = 3
+    DEVICE_TYPE = (
+        (WEB, _('web')),
+        (IOS, _('ios')),
+        (ANDROID, _('android'))
+    )
+
+    user = models.ForeignKey(User, related_name='device', on_delete=models.CASCADE)
+    device_uuid = models.UUIDField(_('Device UUID'), null=True)
+    last_login = models.DateTimeField(_('last login'), blank=True, null=True)
+    device_type = models.PositiveSmallIntegerField(_('Device Type'), choices=DEVICE_TYPE, default=WEB)
+    device_os = models.CharField(_('device os'), max_length=25, blank=True)
+    device_model = models.CharField(_('device model'), max_length=50, blank=True)
+    device_os = models.CharField(_('app version'), max_length=25, blank=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+
+
+    class Meta:
+        db_table = _('devices')
+        verbose_name = _('device')
+        verbose_name_plural = _('devices')
+        unique_together = ('user', 'device_uuid')
+
+
+
+class Province(models.Model):
+    name = models.CharField(max_length=50)
+    is_valid = models.BooleanField(default=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
